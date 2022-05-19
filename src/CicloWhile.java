f2cfa8e5da516fafff4a2468aee6ee8d59f6c40c
Jose Valenzuela <valenz74@gmail.com>
2022-05-18 21:17:08 -0500

    Aprendiendo sobre Ciclos While en Java

---
+++ src/CicloWhile.java
@@ -0,0 +1,18 @@
+public class CicloWhile {
+    public static void main(String[] args) {
+        /*
+           while (expresion booleana) {
+                sentencia
+           }
+        -- Brake
+        -- Continue
+         */
+
+        int i = 300;
+        while (i<10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Valor de i es: " + i);
+    }
+}
